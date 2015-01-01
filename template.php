c104f48d46c15a2b182ad01ab40f9c9e30a5b0d3
freezurbern <freezurbern@gmail.com>
2015-01-01 14:35:32 -0500

    day two progress

---
+++ template.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Crohn's Kitchen</title>
+	<link rel="stylesheet" href="main.css" />
+	<script type="text/javascript" src="features.js"></script>
+	<meta charset="utf-8"/>
+</head>
+<body class="main-page" onload="" >
+	<header>
+		<a href="index.php"><img src="img/logo.png" alt="CK Logo"></a>
+		<div id="header-byline">Main page</div>
+		<div id="header-sidebar">
+			<ul>
+			<li><span class="">Quick Links</span></li>
+			<li><a href="user.php?profile">Profile</a></li>
+			<li><a href="user.php?login">Login</a></li>
+			<li><a href="user.php?logout">Logout</a></li>
+			</ul>
+		</div>
+		<nav>
+			<a href="index.php" id="nav-main" class="button main-navlink">Main</a>
+			<a href="add-meal.php" id="nav-meal" class="button meal-navlink">Add Meal</a>
+			<a href="add-food.php" id="nav-food" class="button food-navlink">Add Food</a>
+			<a href="research.php" id="nav-research" class="button research-navlink">Research</a>
+		</nav>
+	</header>
+
+		<div id="pagecontent">
+			<p>
+				<!-- CONTENT GOES HERE -->
+				<h1>Welcome to Crohn's Kitchen!</h1>
+				<p>Using this site you can keep track irritant foods, analyse meals, and more.</p>
+			</p>
+		</div>
+
+<footer>
+	<hr style="background-color: #222; height: 0.1em;" />
+	<a href="index.php"><img src="img/footer-nav-logo.png" alt="CK Logo"></a>
+	<a href="https://github.com/freezurbern/CrohnsKitchen">&copy; MIT License 2014 freezurbern.com</a>
+	&nbsp;<b>&middot;</b>&nbsp;blahh
+	<b>&middot;</b>&nbsp;
+	Validate <a href="http://validator.w3.org/check?uri=referer">HTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+</footer>
+</body>
+</html>
