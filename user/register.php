6d2149fee325d423d3c403b4caa2f3b6c1f72d0a
freezurbern <freezurbern@gmail.com>
2015-01-13 13:07:39 -0500

    implemented Google's newer reCAPTCHA for certain forms (registration and password reset in future)

--- user/register.php
+++ user/register.php
@@ -10,8 +10,10 @@
 			<input type="text" name="user" size="20" placeholder="Username" required><br>
 		Password:
 			<input type="password" name="pass" size="20" placeholder="Password" required><br>
+		<div class="g-recaptcha" data-sitekey="6LdcaQATAAAAAH6NCoVL4sLAmRlgzuhAHt_ikJG4"></div><br>
 		<input type="hidden" name="op" value="register">
 		<input type="submit" value="Create user">
+		
 	</fieldset>
 	</form>
 
