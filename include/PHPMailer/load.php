cb9b5b62fc9b90154d62121a3ce6961180a0597a
freezurbern <freezurbern@gmail.com>
2015-01-18 09:05:39 -0500

    initial files needed for emailing users

---
+++ include/PHPMailer/load.php
@@ -0,0 +1,9 @@
+<?php
+// require the files
+require('class.phpmailer.php');
+require('class.pop3.php');
+require('class.smtp.php');
+
+require($_SERVER['DOCUMENT_ROOT'] . "/../protected/db_auth.php"); // grab the server connection details.
+
+?>
