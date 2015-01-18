fea580822253fc824ffccb58bfc4c26b50204c3b
freezurbern <freezurbern@gmail.com>
2015-01-17 20:19:30 -0500

    working view_users.php file. shows how to retrieve big select queries.

--- admin/view_users.php
+++ admin/view_users.php
@@ -3,7 +3,6 @@
  * author: freezurbern
  * date: Jan 2015
 */
-echo "hello.";
 require($_SERVER['DOCUMENT_ROOT'] . "/include/check_login.php");
 	if ($ul && $us) {} else { echo "not logged in." ; exit(); }
 require($_SERVER['DOCUMENT_ROOT'] . "/../protected/db_auth.php"); // grab the server connection details.
@@ -31,41 +30,31 @@ if (!mysqli_select_db($db, 'ckdata'))
 	exit();
 }
 
-fail('Server and database connection established.', '');
+//fail('Server and database connection established.', '');
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 /* @@@@@	End DB Setup	@@@@@ */
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 
 /* Get information out of the database, and show it in a template */
-($stmt = $db->prepare('SELECT * FROM users'))
+($stmt = $db->prepare('SELECT uid, user, pass, email, date_registered FROM users'))
 	|| fail('MySQL prepare', $db->error);
 $stmt->execute()
 	|| fail('MySQL execute', $db->error);
-$stmt->bind_result($idarr, $userarr, $passarr, $emailarr)
-	|| fail('MySQL bind_result', $db->error);
+$meta = $stmt->result_metadata();
+while ($field = $meta->fetch_field()) {
+  $parameters[] = &$row[$field->name];
+}
+call_user_func_array(array($stmt, 'bind_result'), $parameters);
+while ($stmt->fetch()) {
+  foreach($row as $key => $val) {
+    $x[$key] = $val;
+  }
+  $results[] = $x;
+}
 if (!$stmt->fetch() && $db->errno)
 	fail('MySQL fetch', $db->error);
-
-if (!$result)
-{
-	fail('Failed.', $result);
-	exit();
-}
-echo '</pre>'; // end prettified for now.
-while ($row = mysqli_fetch_array($result))
-{
-	$userdata[] = array(
-		'uid' => $row['uid'], 
-		'user' => $row['user'], 
-		'email' => $row['email'], 
-		'pass' => $row['pass'], 
-		'date_registered' => $row['date_registered']
-		);
-}
-
-
-
-
+	
+//echo "<hr />";
 ?>
 
 <?php require($_SERVER['DOCUMENT_ROOT'] . "/template/header.php");?>
@@ -77,17 +66,17 @@ while ($row = mysqli_fetch_array($result))
 				<th>Uid</th>
 				<th>User</th>
 				<th>Email</th>
-				<th>Pass</th>
+				<!--<th>Pass</th>-->
 				<th>Date Registered</th>
 			</tr>
 		</thead>
 		<tbody>
-		<?php foreach ($userdata as $curUser): ?>
+		<?php foreach ($results as $curUser): ?>
 			<tr>
 				<td><?php echo htmlspecialchars($curUser['uid'], ENT_QUOTES, 'UTF-8'); ?></td>
 				<td><?php echo htmlspecialchars($curUser['user'], ENT_QUOTES, 'UTF-8'); ?></td>
 				<td><?php echo htmlspecialchars($curUser['email'], ENT_QUOTES, 'UTF-8'); ?></td> 
-				<td><?php echo htmlspecialchars($curUser['pass'], ENT_QUOTES, 'UTF-8'); ?></td>
+				<!--<td><?php echo htmlspecialchars($curUser['pass'], ENT_QUOTES, 'UTF-8'); ?></td>-->
 				<td><?php echo htmlspecialchars($curUser['date_registered'], ENT_QUOTES, 'UTF-8'); ?></td> 
 			</tr>
 		<?php endforeach; ?>
