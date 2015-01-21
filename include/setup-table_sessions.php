b828f83092b5de43b408ccdfa3384a73d18db6fd
freezurbern <freezurbern@gmail.com>
2015-01-21 13:47:34 -0500

    need to redo sessions table creation code to be object oriented, starting.

--- include/setup-table_sessions.php
+++ include/setup-table_sessions.php
@@ -1,20 +1,5 @@
 <?php
 
-// DEBUG MODE: If enabled, OK to leak server setup details.
-$debug = TRUE;
-function fail($pub, $pvt = '')
-{
-	global $debug;
-	$msg = $pub;
-	if ($debug && $pvt !== '')
-		$msg .= ": $pvt";
-/* The $pvt debugging messages may contain characters that would need to be
- * quoted if we were producing HTML output, like we would be in a real app,
- * but we're using text/plain here.  Also, $debug is meant to be disabled on
- * a "production install" to avoid leaking server setup details. */
-	exit("An error occurred ($msg).\n");
-}
-
 require($_SERVER['DOCUMENT_ROOT'] . "/../protected/authcodes.php"); // grab the server connection details.
 
 // construct a table for all the sessions
