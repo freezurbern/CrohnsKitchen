f7af2a62dcab01bb550ecf4ad0a30067b36d7d9a
freezurbern <freezurbern@gmail.com>
2015-01-29 15:53:38 -0500

    getting research page working again

--- template/header.php
+++ template/header.php
@@ -9,6 +9,12 @@
 
 	<!-- Bootstrap -->
 	<link href="/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/js/Chart.min.js"></script>
 </head>
   <nav class="navbar navbar-default">
 	  <div class="container-fluid">
