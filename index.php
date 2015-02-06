a0721bc457c28977ab1e8de016c5c9a7217ff4fd
freezurbern <freezurbern@gmail.com>
2015-02-06 16:20:26 -0500

    form and css work. added help page to match link.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require($_SERVER['DOCUMENT_ROOT'] . "/template/header.php");?>
-<article id="pagecontent">
+<div class="container-fluid">
 	<h2>Hello World! H2 Here!</h2>
 	<p>Here is my paragraph!</p>
-</article>
+</div>
 <?php require($_SERVER['DOCUMENT_ROOT'] . "/template/footer.php");?>
