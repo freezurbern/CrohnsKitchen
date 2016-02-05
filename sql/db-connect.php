1fec59ae6c40e56744a7abe5a8c93ffaa2171d62
freezurbern <freezurbern@gmail.com>
2016-02-05 14:57:15 -0500

    move sql to new folder in root

--- sql/db-connect.php
+++ sql/db-connect.php
@@ -5,7 +5,6 @@
  * Date: 2/5/2016
  * Time: 2:44 PM
  */
-// <?php require($_SERVER['DOCUMENT_ROOT'] . "/include/head-nav.php");?>
 
 include_once($_SERVER['DOCUMENT_ROOT'] . '/dbconfig.inc.php');
 $db = new PDO('mysql:host=localhost;dbname=$config['dbname'];charset=utf8', $config['dbuser'], $config['dbpass']);
