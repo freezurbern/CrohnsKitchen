5c02485498d2dedcb7de23e91f86197acb89ab95
freezurbern <freezurbern@gmail.com>
2016-02-05 21:23:00 -0500

    db-connect

--- sql/db-connect.php
+++ sql/db-connect.php
@@ -1,10 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zachery
- * Date: 2/5/2016
- * Time: 2:44 PM
- */
 
-include_once($_SERVER['DOCUMENT_ROOT'] . '/dbconfig.inc.php');
-$db = new PDO('mysql:host=localhost;dbname=$config['dbname'];charset=utf8', $config['dbuser'], $config['dbpass']);
+require_once($_SERVER['DOCUMENT_ROOT'] . "/dbconfig.inc.php");
+
+try {
+$db = new PDO('mysql:host='.$config['dbhost'].';dbname='.$config['dbname'], $config['dbuser'], $config['dbpass']);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+foreach($db->query('SELECT * FROM users') as $row) {
+    echo 'uID:'.$row['uID'].' email:'.$row['email'];
+}
+$stmt = $db->query('SELECT * FROM users');
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//use $results
+echo '<br>';
+print_r(array_values($results));
+echo '<br>';
+echo $results[0][uid];
+echo $results[0][email];
+// done
+?>
